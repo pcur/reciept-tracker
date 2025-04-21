c362e8b2551246d5089abf53094dadd1b4460ca9
pcur <92280141+pcur@users.noreply.github.com>
2025-04-21 18:37:45 -0400

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,2 +1,20 @@
 from config.env import load_config
 load_config()
+
+import streamlit as st
+from services.db_service import get_all_receipts
+import pandas as pd
+
+st.header("📊 Dashboard")
+
+receipts = get_all_receipts()
+
+if not receipts:
+    st.info("No receipts found yet.")
+
+
+df = pd.DataFrame(receipts)
+st.dataframe(df)
+
+total_spent = sum(receipt["total"] for receipt in receipts)
+st.metric("Total Spent", f"${total_spent:.2f}")
